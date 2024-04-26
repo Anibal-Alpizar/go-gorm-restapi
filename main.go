39f97ceb7a4d750689e0b7a2a93403f68f5c6970
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-04-25 22:18:55 -0600

    chore: Auto-migrate Task and User models in main function

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"net/http"
 
 	"github.com/anibal-alpizar/go-gorm-fastapi/db"
+	"github.com/anibal-alpizar/go-gorm-fastapi/models"
 	"github.com/anibal-alpizar/go-gorm-fastapi/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	db.DBConnection()
+
+	db.DB.AutoMigrate(models.Task{})
+	db.DB.AutoMigrate(models.User{})
 
 	r := mux.NewRouter()
 
