63af4e438a27b7622f42ee3766d030fe9cb6b72d
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-04-27 17:42:50 -0600

    chore: Add CRUD routes for tasks

--- main.go
+++ main.go
@@ -17,11 +17,18 @@ func main() {
 
 	r := mux.NewRouter()
 
+	// User routes
 	r.HandleFunc("/", routes.HomeHandler)
 	r.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
 	r.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
 	r.HandleFunc("/users", routes.CreateUserHandler).Methods("POST")
 	r.HandleFunc("/users/{id}", routes.DeleteUserHandler).Methods("DELETE")
+
+	// Task routes
+	r.HandleFunc("/tasks", routes.GetTasksHandler).Methods("GET")
+	r.HandleFunc("/tasks/{id}", routes.GetTaskHandler).Methods("GET")
+	r.HandleFunc("/tasks", routes.CreateTaskHandler).Methods("POST")
+	r.HandleFunc("/tasks/{id}", routes.DeleteTaskHandler).Methods("DELETE")
 
 	http.ListenAndServe(":8080", r)
 }
--- routes/tasks.routes.go
+++ routes/tasks.routes.go
@@ -1,1 +1,49 @@
 package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/anibal-alpizar/go-gorm-fastapi/db"
+	"github.com/anibal-alpizar/go-gorm-fastapi/models"
+	"github.com/gorilla/mux"
+)
+
+func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	var tasks []models.Task
+	db.DB.Find(&tasks)
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func GetTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	params := mux.Vars(r)
+	db.DB.First(&task, params["id"])
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode("Task not found")
+		return
+	}
+	json.NewEncoder(w).Encode(task)
+}
+
+func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	json.NewDecoder(r.Body).Decode(&task)
+	db.DB.Create(&task)
+	json.NewEncoder(w).Encode(task)
+}
+
+func DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	params := mux.Vars(r)
+	db.DB.First(&task, params["id"])
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode("Task not found")
+		return
+	}
+	// db.DB.Delete(&task) // (logical delete)
+	db.DB.Unscoped().Delete(&task) // (physical delete)
+	w.WriteHeader(http.StatusNoContent) // 204
+}
